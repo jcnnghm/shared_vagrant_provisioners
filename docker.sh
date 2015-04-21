f576e454ba4ae3dfa1044f8449f8c2a73f8deaa0
Justin Cunningham <justin@bulletprooftiger.com>
2015-04-20 21:56:59 -0700

    Added shared docker provisioner

---
+++ docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+[ -e /usr/lib/apt/methods/https ] || {
+  apt-get update
+  apt-get install -y apt-transport-https
+}
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get install -y lxc-docker
+sudo apt-get upgrade -y
+
+sudo groupadd docker
+sudo gpasswd -a vagrant docker
+sudo service docker restart
