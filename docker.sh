e8e3a6d71c1dced9bb4ae9c6325b747df30e0b20
Justin Cunningham <justin@bulletprooftiger.com>
2015-04-20 22:07:46 -0700

    Making apt-get install run in sudo

--- docker.sh
+++ docker.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 [ -e /usr/lib/apt/methods/https ] || {
-  apt-get update
-  apt-get install -y apt-transport-https
+  sudo apt-get update
+  sudo apt-get install -y apt-transport-https
 }
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
